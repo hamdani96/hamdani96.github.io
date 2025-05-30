b331ff213213a8b0a6cbec037e8e024a61694179
hamdani96 <hamdani.hams456@gmail.com>
2025-05-30 18:57:11 +0700

    fix text

--- suprise/js/main.js
+++ suprise/js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('LOVE U SENG').split('')
+    const titles = ('LOVE U').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
