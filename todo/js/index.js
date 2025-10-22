d491c42d19685bf085333bb71f63c2b77c19c54b
hamdani96 <hamdani.hams456@gmail.com>
2025-10-22 17:27:23 +0700

    fix todo

--- todo/js/index.js
+++ todo/js/index.js
@@ -6,66 +6,66 @@ const todoListContainer = document.querySelector("ul");
 
 // Event saat form disubmit
 form.addEventListener("submit", function (e) {
-  e.preventDefault(); // mencegah reload halaman
+    e.preventDefault();
 
-  const taskText = input.value.trim();
-
-  if (taskText === "") {
-    // tampilkan pesan error
-    errorMessage.style.display = "block";
-    return;
-  }
-
-  // sembunyikan error kalau input valid
-  errorMessage.style.display = "none";
-
-  // buat elemen li baru
-  const li = document.createElement("li");
-  li.classList.add("todo-list");
-
-  // buat checkbox
-  const checkbox = document.createElement("input");
-  checkbox.type = "checkbox";
-
-  // buat span untuk teks tugas
-  const span = document.createElement("span");
-  span.classList.add("title-check");
-  span.textContent = taskText;
-
-  // buat tombol hapus
-  const deleteBtn = document.createElement("button");
-  deleteBtn.classList.add("btn-delete");
-  deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
-
-  // tambahkan event untuk checkbox
-  checkbox.addEventListener("change", function () {
-    if (this.checked) {
-      span.style.textDecoration = "line-through";
-      span.style.color = "#aaa";
-    } else {
-      span.style.textDecoration = "none";
-      span.style.color = "black";
+    // ambil value input dan mengambaikan spasi di awal dan di akhir
+    const taskText = input.value.trim();
+                
+    // validasi input kosong
+    if (taskText === "") {
+        errorMessage.style.display = "block";
+        return;
     }
-  });
 
-  // tambahkan event untuk hapus
-  deleteBtn.addEventListener("click", function () {
-    const yakin = confirm("Apakah kamu yakin ingin menghapus tugas ini?");
+    // sembunyikan error kalau input valid
+    errorMessage.style.display = "none";
 
-    if (yakin) {
-      li.remove();
-      alert("Tugas berhasil dihapus!");
-    }
-  });
+    // buat elemen li baru
+    const li = document.createElement("li");
+    li.classList.add("todo-list");
 
-  // gabungkan elemen ke dalam li
-  li.appendChild(checkbox);
-  li.appendChild(span);
-  li.appendChild(deleteBtn);
+    // buat checkbox
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
 
-  // tambahkan ke daftar ul
-  todoListContainer.appendChild(li);
+    // buat span untuk teks tugas
+    const span = document.createElement("span");
+    span.classList.add("title-check");
+    span.textContent = taskText;
 
-  // reset input
-  input.value = "";
+    // buat tombol hapus
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("btn-delete");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+
+    // fungsi untuk menandai tugas selesai
+    checkbox.addEventListener("change", function () {
+        if (this.checked) {
+            span.style.textDecoration = "line-through";
+            span.style.color = "#aaa";
+        } else {
+            span.style.textDecoration = "none";
+            span.style.color = "black";
+        }
+    });
+
+    // muncul alert ketika btn delete di klik
+    deleteBtn.addEventListener("click", function () {
+        const yakin = confirm("Apakah kamu yakin ingin menghapus tugas ini?");
+
+        if (yakin) {
+            li.remove();
+        }
+    });
+
+    // gabungkan elemen ke dalam li
+    li.appendChild(checkbox);
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+
+    // tambahkan ke daftar ul
+    todoListContainer.appendChild(li);
+
+    // reset input
+    input.value = "";
 });
